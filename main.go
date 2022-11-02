8f0c1dadb07e9b75d477e2c2ae34d72ffedd8741
CarsonV <carsonv22@gmail.com>
2022-11-02 01:42:28 +0000

    Multiple regions tracking hardcoded

--- main.go
+++ main.go
@@ -7,17 +7,44 @@ import (
 	"os"
 	"os/signal"
 	"time"
-	//"encoding/json"
+	"encoding/json"
 
 	"github.com/gorilla/websocket"
+	//"github.com/bwmarrin/discordgo"
 )
 
 var addr = flag.String("addr", "zkillboard.com", "http service address")
 
+type zKillSubMsg struct {
+	Action    string `json:"action"`
+	Channel   string `json:"channel"`
+}
+/*
+var (
+	GuildID  = flag.String("guild", "", "Test guild ID")
+	BotToken = flag.String("token", "", "Bot access token")
+	AppID    = flag.String("app", "", "Application ID")
+)
+
+
+var s *discordgo.Session
+
+func zkillLink(s *discordgo.Session) {
+
+}
+*/
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+/*
+	dg, err := discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatal("Invalid bot:", err)
+	}
+	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.ChannelMessageSendComplex
 
+	defer dg.Close()
+*/
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
@@ -45,14 +72,40 @@ func main() {
 		}
 	}()
 	
-	subMsg := []byte("{\"action\":\"sub\",\"channel\":\"region:10000060\"}")
-	log.Println("Subscribing")
-	err = c.WriteMessage(1, subMsg)
-	if err != nil {
-		log.Println("SubError:", err)
-		return
-	}
 
+	/*
+	Delve: 10000060
+	Querious: 10000050
+	Period Basis: 10000063
+	Fountain: 10000058
+	*/
+
+	regionList := make([]string, 4)
+	regionList[0] = "region:10000060"
+	regionList[1] = "region:10000050"
+	regionList[2] = "region:10000063"
+	regionList[3] = "region:10000058"
+
+
+	for i := range regionList {
+		regionSub := zKillSubMsg{
+			Action: "sub",
+			Channel: regionList[i],
+		}
+	
+		newSub, err := json.Marshal(regionSub)
+		if err != nil {
+			log.Println("marshal: ", err)
+		}
+		log.Println(string(newSub))
+		log.Println("Subscribing: ", i)
+		
+		err = c.WriteMessage(1, newSub)
+		if err != nil {
+			log.Println("SubError:", err)
+			return
+		}
+	}
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
