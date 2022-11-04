2cb2dfc13b2f118c5e538fdea1b74b0c52555c47
CarsonV <carsonv22@gmail.com>
2022-11-04 02:07:12 +0000

    Discord setup and zKB response block

--- main.go
+++ main.go
@@ -1,50 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/url"
 	"os"
 	"os/signal"
 	"time"
-	"encoding/json"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/gorilla/websocket"
-	//"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
+// TODO: make zkill response struct like other.
+// check/make bot functionality
 var addr = flag.String("addr", "zkillboard.com", "http service address")
 
 type zKillSubMsg struct {
-	Action    string `json:"action"`
-	Channel   string `json:"channel"`
+	Action  string `json:"action"`
+	Channel string `json:"channel"`
 }
-/*
-var (
-	GuildID  = flag.String("guild", "", "Test guild ID")
-	BotToken = flag.String("token", "", "Bot access token")
-	AppID    = flag.String("app", "", "Application ID")
-)
 
+type zKBlock struct {
+	Action      string `json:"action"`
+	KillID      int    `json:"killID"`
+	CharID      int    `json:"character_id"`
+	CorpID      int    `json:"corporation_id"`
+	AllianceID  int    `json:"alliance_id"`
+	ShipID      int    `json:"ship_type_id"`
+	ShipGroupID int    `json:"group_id"`
+	URL         string `json:"url"`
+	Hash        string `json:"hash"`
+	SubChan     string `json:"channel"`
+}
 
 var s *discordgo.Session
 
 func zkillLink(s *discordgo.Session) {
 
 }
-*/
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	discordToken := os.Getenv("BOT_TOKEN")
+	//guildToken := os.Getenv("GUILD_TOKEN")
+	//appToken := os.Getenv("APP_TOKEN")
+
 	flag.Parse()
 	log.SetFlags(0)
-/*
-	dg, err := discordgo.New("Bot " + *BotToken)
+
+	s, err := discordgo.New("Bot " + discordToken)
 	if err != nil {
 		log.Fatal("Invalid bot:", err)
 	}
 	//https://pkg.go.dev/github.com/bwmarrin/discordgo#Session.ChannelMessageSendComplex
+	err = s.Open()
+	if err != nil {
+		log.Fatal("Discord: ", err)
+	}
 
-	defer dg.Close()
-*/
+	defer s.Close()
+
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
@@ -71,13 +93,12 @@ func main() {
 			log.Printf("recv: %s", message)
 		}
 	}()
-	
 
 	/*
-	Delve: 10000060
-	Querious: 10000050
-	Period Basis: 10000063
-	Fountain: 10000058
+		Delve: 10000060
+		Querious: 10000050
+		Period Basis: 10000063
+		Fountain: 10000058
 	*/
 
 	regionList := make([]string, 4)
@@ -86,26 +107,27 @@ func main() {
 	regionList[2] = "region:10000063"
 	regionList[3] = "region:10000058"
 
-
 	for i := range regionList {
 		regionSub := zKillSubMsg{
-			Action: "sub",
+			Action:  "sub",
 			Channel: regionList[i],
 		}
-	
+
 		newSub, err := json.Marshal(regionSub)
 		if err != nil {
 			log.Println("marshal: ", err)
 		}
 		log.Println(string(newSub))
 		log.Println("Subscribing: ", i)
-		
+
 		err = c.WriteMessage(1, newSub)
 		if err != nil {
 			log.Println("SubError:", err)
 			return
 		}
 	}
+
+	//below keeps the websocket goroutine running until clean shutdown or connection close.
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
